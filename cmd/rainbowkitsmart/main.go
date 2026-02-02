ee96b5c37310780a01fe549b641c3c646be13be3
Cze <120347462+czerid@users.noreply.github.com>
2026-02-02 16:07:17 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/rainbowkitsmart/main.go
@@ -0,0 +1,23 @@
+// cmd/rainbowkitsmart/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "rainbowkitsmart/internal/rainbowkitsmart"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := rainbowkitsmart.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
